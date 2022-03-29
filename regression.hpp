48b2917759b4fa5332bf604530e1490a5df5bd13
Elena Rainer <elena.rainer02@gmail.com>
2022-03-29 19:50:49 +0200

    Use vector of 2D points in Regression

--- regression.hpp
+++ regression.hpp
@@ -1,11 +1,20 @@
 #ifndef REGRESSION_HPP
 #define REGRESSION_HPP
+
+#include <vector>
+
 struct Result {
   double A;
   double B;
 };
 
 class Regression {
+  struct Point {
+    double x;
+    double y;
+  };
+
+  std::vector<Point> points_{};
   int N_{};
   double sum_x_{};
   double sum_y_{};
